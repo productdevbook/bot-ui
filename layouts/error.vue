9909f6dcb9358877e0e5a97839a9ca2cd7d6a0b6
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2021-05-15 00:50:47 +0200

    Add custom error page component

---
+++ layouts/error.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="bg-gray-800 min-w-1/4 min-h-1/4 flex flex-col justify-center items-center">
+    <h1 v-if="error.statusCode === 404">Page not found</h1>
+    <h1 v-else>An error occurred - {{ error.statusCode }}</h1>
+    <button @click="$router.replace('/')">Home page</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+
+@Component({
+  props: ['error'],
+  layout: 'login',
+  transition: 'default'
+})
+export default class ErrorPage extends Vue {}
+</script>
