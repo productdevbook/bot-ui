ca14e26f49c3b506c7aadfe369d908771281fd4c
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2021-05-15 16:56:41 +0200

    Remove start file

--- server-middleware/auth/start.ts
+++
@@ -1,6 +0,0 @@
-import app from './index';
-
-// on Heroku if trying to serve "full-static" site we need to spin up express for the server middleware to work
-if (process.env.RENDER_TARGET === 'static') {
-  app.listen(process.env.PORT);
-}
