08e2f57ac81a724059ab8b09272dfd845cc034b6
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2021-05-16 00:13:14 +0200

    Add onError check to apollo config

--- plugins/apollo/config.ts
+++ plugins/apollo/config.ts
@@ -1,12 +1,17 @@
 import { onError } from '@apollo/client/link/error';
 import { Context } from '@nuxt/types';
 
-export default function ({ $config }: Context) {
+export default function ({ $config, $apolloHelpers, $toast, redirect }: Context) {
   const httpEndpoint = $config.graphQlEndpoint;
 
   const link = onError(({ graphQLErrors }) => {
     graphQLErrors?.forEach((err) => {
       console.log(err);
+      if (err.message === 'Authentication failed!') {
+        $apolloHelpers.onLogout();
+        $toast.error('Authentication issue. Please log in again.');
+        redirect('/login');
+      }
     });
   });
 
