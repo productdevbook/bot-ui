21344fd8d8fb8f2bff26a61858c5a2de73e2d7f2
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2021-05-23 16:07:18 +0200

    Update Apollo onError link config

--- plugins/apollo/config.ts
+++ plugins/apollo/config.ts
@@ -1,17 +1,16 @@
 import { onError } from '@apollo/client/link/error';
 import { Context } from '@nuxt/types';
 
-export default function ({ $config, $apolloHelpers, $toast, redirect }: Context) {
+export default function ({ $config, $toast, redirect, route }: Context) {
   const httpEndpoint = $config.graphQlEndpoint;
 
   const link = onError(({ graphQLErrors }) => {
     graphQLErrors?.forEach((err) => {
-      console.log(err);
-      if (err.message === 'Authentication failed!') {
-        $apolloHelpers.onLogout();
-        $toast.error('Authentication issue. Please log in again.');
-        redirect('/login');
+      console.debug(err);
+      if (err.message === 'Authentication failed.') {
+        if (route.name !== 'login') redirect('/login');
       }
+      $toast.error(err.message);
     });
   });
 
