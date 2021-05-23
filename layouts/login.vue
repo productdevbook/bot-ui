03a16e7f4b5c156a9757ce2bc63554108c94bb5b
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2021-05-23 16:04:35 +0200

    Add vuetify layout to login layout component No header or footer for login page, just a centered card with a login form

--- layouts/login.vue
+++ layouts/login.vue
@@ -1,7 +1,11 @@
 <template>
-  <div id="app">
-    <Nuxt />
-  </div>
+  <v-app id="app" dark>
+    <v-main>
+      <v-container fill-height fluid>
+        <Nuxt keep-alive :keep-alive-props="{ max: 10 }" />
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 <script lang="ts">
 import '@/assets/scss/app.scss';
