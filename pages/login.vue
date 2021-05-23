9d26ccc17a4d080ec829b0121735661de0dc6b67
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2021-05-23 16:06:33 +0200

    Update login page to be a bit more user friendly Successful login shows a small icon before transition Failed Login shows an icon too Login is deferred by a second for the icon to pop up

--- pages/login.vue
+++ pages/login.vue
@@ -4,10 +4,18 @@
       <v-card elevation="10" :loading="loading" outlined shaped>
         <v-card-title> Welcome</v-card-title>
         <v-card-subtitle> This page is being reworked. Please be patient until it's finished.</v-card-subtitle>
-        <v-card-text>
-          <v-form ref="form" v-model="valid">
-            <v-text-field v-model="username" :rules="rules" label="Username" required></v-text-field>
-            <v-text-field v-model="password" type="password" :rules="rules" label="Password" required></v-text-field>
+        <v-card-text class="justify-center">
+          <v-form ref="form" v-model="valid" :disabled="loading" @submit.prevent="submit">
+            <v-text-field v-model="username" :rules="rules" label="Username" required @input="error = false"></v-text-field>
+            <v-text-field
+              v-model="password"
+              type="password"
+              :rules="rules"
+              label="Password"
+              required
+              @input="error = false"
+            ></v-text-field>
+            <v-btn style="display: none" type="submit"></v-btn>
           </v-form>
           <transition name="fade" mode="out-in">
             <p v-show="error" class="red--text font-weight-bold">There was an issue while logging you in. Please try again.</p>
@@ -16,9 +24,12 @@
         <v-card-actions class="justify-center">
           <v-btn :disabled="!valid" :loading="loading" outlined color="primary" @click="submit">
             <transition name="fade" mode="out-in">
-              <template v-if="!success"><span>Login</span></template>
+              <template v-if="!success && !loading && !error"><span>Login</span></template>
+              <template v-else-if="success">
+                <v-icon color="green" dark> mdi-check-outline</v-icon>
+              </template>
               <template v-else>
-                <v-icon color="green" dark> mdi-check-outline </v-icon>
+                <v-icon color="red" dark>mdi-cancel</v-icon>
               </template>
             </transition>
           </v-btn>
@@ -29,9 +40,11 @@
 </template>
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator';
+import { timer } from 'rxjs';
 
 @Component({
   name: 'LoginForm',
+  layout: 'login',
   transition: 'default'
 })
 export default class LoginForm extends Vue {
@@ -52,8 +65,9 @@ export default class LoginForm extends Vue {
     try {
       const login = await this.$sm.login(this.username, this.password);
       if (login) {
-        await this.$store.dispatch('user/login', { id: '0', username: this.username, roles: ['user'] });
-        return await this.$router.replace('/');
+        this.loading = false;
+        this.success = true;
+        return timer(1000).subscribe(async () => await this.$router.replace('/'));
       }
     } catch (e) {
       console.log(e);
