e2b45a7fadb9f3d212f30c6600772fe50056945c
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2021-05-23 16:05:08 +0200

    Remove stage condition from auth middleware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,19 +1,17 @@
 import { Context, Middleware } from '@nuxt/types';
 
 // Check if user has a JWT token present, if not redirect back to login
-const AuthMiddleware: Middleware = ({ route, redirect, app: { $apolloHelpers }, $config }: Context) => {
-  if ($config.stage === 'dev') {
-    const requiresAuth = !route.name?.includes('login');
-    const hasToken = !!$apolloHelpers.getToken();
-    if (requiresAuth) {
-      if (!hasToken) {
-        $apolloHelpers.onLogout();
-        redirect('/login');
-      }
-    } else if (!requiresAuth) {
-      if (hasToken) {
-        redirect('/');
-      }
+const AuthMiddleware: Middleware = ({ route, redirect, app: { $apolloHelpers } }: Context) => {
+  const requiresAuth = !route.name?.includes('login');
+  const hasToken = !!$apolloHelpers.getToken();
+  if (requiresAuth) {
+    if (!hasToken) {
+      $apolloHelpers.onLogout();
+      redirect('/login');
+    }
+  } else if (!requiresAuth) {
+    if (hasToken) {
+      redirect('/');
     }
   }
 };
