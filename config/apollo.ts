56a592b514bb4bac206cc8697c0351181cd8646e
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2021-06-17 18:35:34 +0200

    bugfix(auth): Reset user data and apollo-token when login is mounted (after route guard) * add token expiry to apollo config

--- config/apollo.ts
+++ config/apollo.ts
@@ -1,14 +1,15 @@
+import { NuxtApolloConfiguration } from '@nuxtjs/apollo/types/nuxt';
+
 export const NuxtApolloConfig = {
   clientConfigs: {
     default: {
       httpEndpoint: process.env.GRAPHQL_ENDPOINT,
       inMemoryCacheOptions: {
         addTypename: false
-      }
+      },
+      tokenName: 'apollo-token'
     }
   },
-  watchLoading: '~/plugins/apollo/loading.ts',
-  errorHandler: '~/plugins/apollo/error.ts',
   defaultOptions: {
     // See 'apollo' definition
     // For example: default query options
@@ -16,5 +17,6 @@ export const NuxtApolloConfig = {
       loadingKey: 'loading',
       fetchPolicy: 'cache-and-network'
     }
-  }
-};
+  },
+  tokenExpires: process.env.REFRESH_INTERVAL || 300
+} as NuxtApolloConfiguration;
