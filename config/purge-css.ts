059a45ab97b0fdf2951dcb6d39a1c7d7a5d2addc
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2021-06-17 22:51:30 +0200

    feat: fix purgecss purging vuetify too much (added some whitelistings)

--- config/purge-css.ts
+++ config/purge-css.ts
@@ -1,7 +1,20 @@
 export const PurgeCssConfig = {
   mode: 'webpack',
   enabled: process.env.TARGET_STAGE !== 'dev',
-  whitelistPatterns: () => [/^v-((?!application).)*$/, /^\.theme--dark*/, /.*-transition/, /.*-fade/, /.*-slide/, /col*/, /row*/],
+  whitelistPatterns: () => [
+    /^v-((?!application).)*$/,
+    /^v-((?!tab).)*$/,
+    /^v-((?!slide).)*$/,
+    /^v-((?!avatar).)*$/,
+    /^v-((?!btn).)*$/,
+    /^v-((?!item).)*$/,
+    /^\.theme--dark*/,
+    /.*-transition/,
+    /.*-fade/,
+    /.*-slide/,
+    /col*/,
+    /row*/
+  ],
   paths: [
     'node_modules/@nuxtjs/vuetify/**/*.ts',
     'node_modules/@nuxt/vue-app/template/**/*.html',
@@ -9,7 +22,7 @@ export const PurgeCssConfig = {
   ],
   whitelistPatternsChildren: [/^v-((?!application).)*$/, /^theme--dark*/],
   styleExtensions: ['.css'],
-  whitelist: ['body', 'html', 'nuxt-progress', 'v-card', 'v-application', 'v-application--wrap'],
+  whitelist: ['body', 'html', 'nuxt-progress', 'v-card', 'v-application', 'v-application--wrap', 'v-tabs'],
   extractors: [
     {
       extractor: (content) => content.match(/[A-z0-9-:\\/]+/g) || [],
