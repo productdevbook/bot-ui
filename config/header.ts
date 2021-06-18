2bdae64497b74c91a5119596a68f957e8093adaf
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2021-06-18 17:33:32 +0200

    feat(seo): Add some meta tags for seo

--- config/header.ts
+++ config/header.ts
@@ -1,14 +1,36 @@
 import type { NuxtConfig } from '@nuxt/types';
 
 export const NuxtHeaderConfig: NuxtConfig['head'] = {
-  title: '@braks/bot-ui',
+  title: '@braks bot-ui',
   htmlAttrs: {
     lang: 'en'
   },
   meta: [
     { charset: 'utf-8' },
     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-    { hid: 'description', name: 'description', content: 'Create fun chatbots!' }
+    { hid: 'description', name: 'description', content: 'Create your ideas and have fun with @braks bots.' },
+    { hid: 'og:title', property: 'og:title', content: '@braks bot-ui' },
+    {
+      hid: 'og:description',
+      property: 'og:description',
+      content: 'Create your ideas and have fun with @braks bots.'
+    },
+    { hid: 'og:image', property: 'og:image', content: '~/static/icon.png' },
+    {
+      hid: 'og:site_name',
+      property: 'og:site_name',
+      content: 'braks-bot.vercel.app'
+    },
+    {
+      hid: 'og:locale',
+      property: 'og:locale',
+      content: 'en'
+    },
+    {
+      hid: 'og:image:type',
+      property: 'og:image:type',
+      content: 'image/png'
+    }
   ],
   link: [
     { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
